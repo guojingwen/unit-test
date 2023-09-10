c0770f41ca85adcae6b5429cc68314ad960810d5
guojw <2315162186@qq.com>
2023-09-10 16:45:15 +0800

    init

--- matchers.test.js
+++
@@ -1,8 +0,0 @@
-test("toBe匹配器", () => {
-  // toBe  相当于 Object.is
-  expect(10).toBe(10);
-});
-test("toEqual  比较对象", () => {
-  const a = {name: 'zs'}
-  expect({name: 'zs'}).toEqual(a);
-});
--- math.test.ts
+++
@@ -1,8 +0,0 @@
-import { add, minus } from "./math";
-
-test("3+7", () => {
-  expect(add(3, 7)).toBe(10);
-});
-test("7-3", () => {
-  expect(minus(7, 3)).toBe(4);
-});
